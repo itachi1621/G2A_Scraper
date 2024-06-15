0b5d052ad85d894853c7ba3a4869eda25efaf99b
Zero <zero0396@gmail.com>
2024-06-14 23:53:59 -0400

    small fix to multiple links

--- custom_functions/scrapping_funcs.py
+++ custom_functions/scrapping_funcs.py
@@ -63,7 +63,7 @@ def scrape_site_pages(g2a_config,implicit_wait_time:int=10,timer_wait_time:int=6
                 print("Try number: ",j+1)
                 user_agent = UserAgent()
                 options = Options()
-                options.add_argument('-headless')
+                # options.add_argument('-headless')
                 options.add_argument('--no-sandbox')
                 options.add_argument('--disable-dev-shm-usage')
                 #user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
